3c3fef605ce14a540364e9f57febbfb5caa1df53
mitchellsundt <mitchellsundt@gmail.com>
2012-04-03 15:57:25 -0700

    Update to 1.0.2 production.

--- src/org/opendatakit/briefcase/model/BriefcasePreferences.java
+++ src/org/opendatakit/briefcase/model/BriefcasePreferences.java
@@ -23,7 +23,7 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class BriefcasePreferences {
   
-  public static final String VERSION = "v1.0.1 Production";
+  public static final String VERSION = "v1.0.2 Production";
 
   public static void setBriefcaseDirectoryProperty(String value) {
     if ( value == null ) {
