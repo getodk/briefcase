6023dd7b86e412eff98a756de69d90ed701e1ee3
mitchellsundt <mitchellsundt@gmail.com>
2013-09-23 17:20:29 -0700

    Update to the newest javarosa library

--- src/org/opendatakit/briefcase/model/BriefcasePreferences.java
+++ src/org/opendatakit/briefcase/model/BriefcasePreferences.java
@@ -23,7 +23,7 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class BriefcasePreferences {
 
-  public static final String VERSION = "v1.3.2 Production";
+  public static final String VERSION = "v1.4 Production";
 
   public static void setBriefcaseDirectoryProperty(String value) {
     if ( value == null ) {
