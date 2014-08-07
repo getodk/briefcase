1db3bb2ad4290b7ff4f95df5539c2d9c2326d6a5
mitchellsundt <mitchellsundt@gmail.com>
2014-08-07 15:57:23 -0700

    Update version string and add copyright to new file.

--- src/org/opendatakit/briefcase/model/BriefcasePreferences.java
+++ src/org/opendatakit/briefcase/model/BriefcasePreferences.java
@@ -23,7 +23,7 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class BriefcasePreferences {
 
-  public static final String VERSION = "JHU-Briefcase-RC2";
+  public static final String VERSION = "v1.4.4 Production";
 
   public static void setBriefcaseDirectoryProperty(String value) {
     if ( value == null ) {
--- src/org/opendatakit/briefcase/ui/BriefcaseCLI.java
+++ src/org/opendatakit/briefcase/ui/BriefcaseCLI.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2014 University of Washington.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 
 package org.opendatakit.briefcase.ui;
 
@@ -44,6 +60,13 @@ import org.opendatakit.briefcase.util.ServerConnectionTest;
 import org.opendatakit.briefcase.util.TransferFromODK;
 import org.opendatakit.briefcase.util.TransferFromServer;
 
+/**
+ * 
+ * Command line interface contributed by Nafundi
+ * 
+ * @author chartung@nafundi.com
+ *
+ */
 public class BriefcaseCLI {
 
     private CommandLine mCommandline;
