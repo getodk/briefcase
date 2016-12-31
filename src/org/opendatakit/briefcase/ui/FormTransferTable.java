7f7b0d7e4c6bf395f825842f89786384d277a473
anakiou <kiourkosa@gmail.com>
2016-12-31 04:47:00 +0100

    Removed setForeground from Details button in FormTransferTable Fixes #25" (#46)

--- src/org/opendatakit/briefcase/ui/FormTransferTable.java
+++ src/org/opendatakit/briefcase/ui/FormTransferTable.java
@@ -58,10 +58,8 @@ public class FormTransferTable extends JTable {
     @Override public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
       JButton button = (JButton)value;
       if (isSelected) {
-        button.setForeground(table.getSelectionForeground());
         button.setBackground(table.getSelectionBackground());
       } else {
-        button.setForeground(table.getForeground());
         button.setBackground(UIManager.getColor("Button.background"));
       }
       return button;   
