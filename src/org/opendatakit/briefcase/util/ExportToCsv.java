1c6d29187f9212fab49befd3176ec60ab44fdb7f
Hélène Martin <lognaturel@gmail.com>
2017-03-25 21:51:03 -0400

    Replace tabs with spaces

--- src/org/opendatakit/briefcase/util/ExportToCsv.java
+++ src/org/opendatakit/briefcase/util/ExportToCsv.java
@@ -139,32 +139,32 @@ public class ExportToCsv implements ITransformFormAction {
     // Sorts the instances by the submission date. If no submission date, we
     // assume it to be latest.
     if (instances != null) {
-    	Arrays.sort(instances, new Comparator<File>() {
-    		public int compare(File f1, File f2) {
-    			try {
-    				if (f1.isDirectory() && f2.isDirectory()) {
-    					File submission1 = new File(f1, "submission.xml");
-    					String submissionDate1String = XmlManipulationUtils.parseXml(submission1).getRootElement()
-    							.getAttributeValue(null, "submissionDate");
+      Arrays.sort(instances, new Comparator<File>() {
+        public int compare(File f1, File f2) {
+          try {
+            if (f1.isDirectory() && f2.isDirectory()) {
+              File submission1 = new File(f1, "submission.xml");
+              String submissionDate1String = XmlManipulationUtils.parseXml(submission1).getRootElement()
+                  .getAttributeValue(null, "submissionDate");
 
-    					File submission2 = new File(f2, "submission.xml");
-    					String submissionDate2String = XmlManipulationUtils.parseXml(submission2).getRootElement()
-    							.getAttributeValue(null, "submissionDate");
+              File submission2 = new File(f2, "submission.xml");
+              String submissionDate2String = XmlManipulationUtils.parseXml(submission2).getRootElement()
+                  .getAttributeValue(null, "submissionDate");
 
-    					Date submissionDate1 = StringUtils.isNotEmptyNotNull(submissionDate1String)
-    							? WebUtils.parseDate(submissionDate1String) : new Date();
-    							Date submissionDate2 = StringUtils.isNotEmptyNotNull(submissionDate2String)
-    									? WebUtils.parseDate(submissionDate2String) : new Date();
-    									return submissionDate1.compareTo(submissionDate2);
-    				}
-    			} catch (ParsingException e) {
-    				e.printStackTrace();
-    			} catch (FileSystemException e) {
-    				e.printStackTrace();
-    			}
-    			return 0;
-    		}
-    	});
+              Date submissionDate1 = StringUtils.isNotEmptyNotNull(submissionDate1String)
+                  ? WebUtils.parseDate(submissionDate1String) : new Date();
+              Date submissionDate2 = StringUtils.isNotEmptyNotNull(submissionDate2String)
+                  ? WebUtils.parseDate(submissionDate2String) : new Date();
+              return submissionDate1.compareTo(submissionDate2);
+            }
+          } catch (ParsingException e) {
+            e.printStackTrace();
+          } catch (FileSystemException e) {
+            e.printStackTrace();
+          }
+          return 0;
+        }
+      });
     }
 
     for (File instanceDir : instances) {
