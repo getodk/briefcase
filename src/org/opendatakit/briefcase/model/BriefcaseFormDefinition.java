7ab552146d03f78e3a167f34477848af062fc11f
Win Ribeka <nyoman.ribeka@gmail.com>
2017-06-23 14:38:57 -0400

    Adding map to hold already parsed form file - #82.

--- src/org/opendatakit/briefcase/model/BriefcaseFormDefinition.java
+++ src/org/opendatakit/briefcase/model/BriefcaseFormDefinition.java
@@ -22,6 +22,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Serializable;
 import java.security.PrivateKey;
 
 import org.apache.commons.io.FileUtils;
@@ -35,7 +36,7 @@ import org.opendatakit.briefcase.util.BadFormDefinition;
 import org.opendatakit.briefcase.util.FileSystemUtils;
 import org.opendatakit.briefcase.util.JavaRosaParserWrapper;
 
-public class BriefcaseFormDefinition implements IFormDefinition {
+public class BriefcaseFormDefinition implements IFormDefinition, Serializable {
 
   private static final Log log = LogFactory.getLog(BriefcaseFormDefinition.class);
   private final File formFolder;
