3a46637d194b9de9d8751d86c9439c82e0c0dcec
Brent Atkinson <brent.atkinson@gmail.com>
2017-07-17 11:29:23 +0100

    Fixed unused imports flagged by checkstyle

--- src/org/opendatakit/common/utils/WebUtils.java
+++ src/org/opendatakit/common/utils/WebUtils.java
@@ -16,7 +16,6 @@ package org.opendatakit.common.utils;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.ParsePosition;
