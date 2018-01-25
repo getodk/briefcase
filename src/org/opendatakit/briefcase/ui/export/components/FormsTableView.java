7730fea412fb73a5a1f51a712deb0f3e124464d2
Guille <ggalmazor@gmail.com>
2018-01-25 19:19:38 +0100

    Review widths of cols and make central cols resizable on the Export tab

--- src/org/opendatakit/briefcase/ui/export/components/FormsTableView.java
+++ src/org/opendatakit/briefcase/ui/export/components/FormsTableView.java
@@ -49,24 +49,29 @@ public class FormsTableView extends JTable {
 
     addMouseListener(new MouseListenerBuilder().onClick(this::relayClickToButton).build());
 
-    Dimension checkboxDims = getHeaderDimension(HEADERS[SELECTED_CHECKBOX_COL]);
-    Dimension detailButtonDims = model.buildDetailButton(null).getPreferredSize();
-    Dimension lastExportDims = new JLabel("Not exported yet").getPreferredSize();
+    Dimension selectedCheckboxDims = getHeaderDimension(HEADERS[SELECTED_CHECKBOX_COL]);
     Dimension overrideConfButtonDims = model.buildOverrideConfButton(null).getPreferredSize();
+    Dimension formNameDims = getHeaderDimension(HEADERS[FORM_NAME_COL]);
+    Dimension exportStatusDims = getHeaderDimension(HEADERS[EXPORT_STATUS_COL]);
+    Dimension lastExportDims = getHeaderDimension(HEADERS[LAST_EXPORT_COL]);
+    Dimension detailButtonDims = model.buildDetailButton(null).getPreferredSize();
 
     setRowHeight(detailButtonDims.height);
 
     TableColumnModel columns = getColumnModel();
-    columns.getColumn(SELECTED_CHECKBOX_COL).setMinWidth(checkboxDims.width);
-    columns.getColumn(SELECTED_CHECKBOX_COL).setMaxWidth(checkboxDims.width);
-    columns.getColumn(SELECTED_CHECKBOX_COL).setPreferredWidth(checkboxDims.width);
+    columns.getColumn(SELECTED_CHECKBOX_COL).setMinWidth(selectedCheckboxDims.width + 25);
+    columns.getColumn(SELECTED_CHECKBOX_COL).setMaxWidth(selectedCheckboxDims.width + 25);
+    columns.getColumn(SELECTED_CHECKBOX_COL).setPreferredWidth(selectedCheckboxDims.width + 25);
     columns.getColumn(OVERRIDE_CONF_COL).setCellRenderer(cellWithButton());
-    columns.getColumn(OVERRIDE_CONF_COL).setMinWidth(overrideConfButtonDims.width + 5);
-    columns.getColumn(OVERRIDE_CONF_COL).setMaxWidth(overrideConfButtonDims.width + 5);
-    columns.getColumn(OVERRIDE_CONF_COL).setPreferredWidth(overrideConfButtonDims.width + 5);
-    columns.getColumn(LAST_EXPORT_COL).setMinWidth(lastExportDims.width + 10);
-    columns.getColumn(LAST_EXPORT_COL).setMaxWidth(lastExportDims.width + 10);
-    columns.getColumn(LAST_EXPORT_COL).setPreferredWidth(lastExportDims.width + 10);
+    columns.getColumn(OVERRIDE_CONF_COL).setMinWidth(overrideConfButtonDims.width + 15);
+    columns.getColumn(OVERRIDE_CONF_COL).setMaxWidth(overrideConfButtonDims.width + 15);
+    columns.getColumn(OVERRIDE_CONF_COL).setPreferredWidth(overrideConfButtonDims.width + 15);
+    columns.getColumn(FORM_NAME_COL).setMinWidth(formNameDims.width + 25);
+    columns.getColumn(FORM_NAME_COL).setPreferredWidth(formNameDims.width + 25);
+    columns.getColumn(EXPORT_STATUS_COL).setMinWidth(exportStatusDims.width + 25);
+    columns.getColumn(EXPORT_STATUS_COL).setPreferredWidth(exportStatusDims.width + 25);
+    columns.getColumn(LAST_EXPORT_COL).setMinWidth(lastExportDims.width + 25);
+    columns.getColumn(LAST_EXPORT_COL).setPreferredWidth(lastExportDims.width + 25);
     columns.getColumn(DETAIL_BUTTON_COL).setCellRenderer(cellWithButton());
     columns.getColumn(DETAIL_BUTTON_COL).setMinWidth(detailButtonDims.width);
     columns.getColumn(DETAIL_BUTTON_COL).setMaxWidth(detailButtonDims.width);
