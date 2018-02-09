178c2a1358c7eb07577c5d276de0427cd94e1c69
Guille <ggalmazor@gmail.com>
2018-02-09 10:38:09 +0100

    Remove duplicated event subscribers

--- src/org/opendatakit/briefcase/ui/BriefcaseCLI.java
+++ src/org/opendatakit/briefcase/ui/BriefcaseCLI.java
@@ -27,15 +27,7 @@ import java.util.Optional;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.bushe.swing.event.annotation.EventSubscriber;
 import org.opendatakit.aggregate.parser.BaseFormParserForJavaRosa;
-import org.opendatakit.briefcase.model.ExportFailedEvent;
-import org.opendatakit.briefcase.model.ExportProgressEvent;
-import org.opendatakit.briefcase.model.ExportSucceededEvent;
-import org.opendatakit.briefcase.model.FormStatusEvent;
-import org.opendatakit.briefcase.model.RetrieveAvailableFormsFailedEvent;
-import org.opendatakit.briefcase.model.TransferFailedEvent;
-import org.opendatakit.briefcase.model.TransferSucceededEvent;
 
 /**
  * Command line interface contributed by Nafundi
@@ -89,40 +81,4 @@ public class BriefcaseCLI {
       System.exit(1);
     }
   }
-
-  @EventSubscriber(eventClass = ExportProgressEvent.class)
-  public void progress(ExportProgressEvent event) {
-    log.info(event.getText());
-  }
-
-  @EventSubscriber(eventClass = ExportFailedEvent.class)
-  public void failedCompletion(ExportFailedEvent event) {
-    log.error("Failed.");
-  }
-
-  @EventSubscriber(eventClass = TransferFailedEvent.class)
-  public void failedCompletion(TransferFailedEvent event) {
-    log.error("Transfer Failed");
-  }
-
-  @EventSubscriber(eventClass = ExportSucceededEvent.class)
-  public void successfulCompletion(ExportSucceededEvent event) {
-    log.info("Succeeded.");
-  }
-
-  @EventSubscriber(eventClass = TransferSucceededEvent.class)
-  public void successfulCompletion(TransferSucceededEvent event) {
-    log.info("Transfer Succeeded");
-  }
-
-  @EventSubscriber(eventClass = FormStatusEvent.class)
-  public void updateDetailedStatus(FormStatusEvent fse) {
-    log.info(fse.getStatusString());
-  }
-
-  @EventSubscriber(eventClass = RetrieveAvailableFormsFailedEvent.class)
-  public void formsAvailableFromServer(RetrieveAvailableFormsFailedEvent event) {
-    log.error("Accessing the server failed with error: " + event.getReason());
-  }
-
 }
