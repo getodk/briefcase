a42f932c5b5bb5020fb75239088834d9882183da
Guille <ggalmazor@gmail.com>
2018-03-22 12:18:14 +0100

    Add some aesthetic changes to Cli

--- src/org/opendatakit/common/cli/Cli.java
+++ src/org/opendatakit/common/cli/Cli.java
@@ -54,6 +54,17 @@ public class Cli {
     register(Operation.of(SHOW_VERSION, args -> printVersion()));
   }
 
+  /**
+   * Marks a Param for deprecation and assigns an alternative operation.
+   *
+   * When Briefcase detects this param, it will show a message, output the help and
+   * exit with a non-zero status
+   *
+   * @param oldParam the {@link Param} to mark as deprecated
+   * @param alternative the alternative {@link Operation} that Briefcase will suggest to be
+   *                    used instead of the deprecated Param
+   * @return self {@link Cli} instance to chain more method calls
+   */
   public Cli deprecate(Param<?> oldParam, Operation alternative) {
     operations.add(Operation.of(oldParam, __ -> {
       log.warn("Trying to run deprecated param -{}", oldParam.shortCode);
@@ -158,8 +169,8 @@ public class Cli {
   private void checkForMissingParams(CommandLine cli, Set<Param> paramsToCheck) {
     Set<Param> missingParams = paramsToCheck.stream().filter(param -> !cli.hasOption(param.shortCode)).collect(toSet());
     if (!missingParams.isEmpty()) {
-      System.out.printf("Missing params: ");
-      System.out.printf(missingParams.stream().map(param -> "-" + param.shortCode).collect(joining(", ")));
+      System.out.print("Missing params: ");
+      System.out.print(missingParams.stream().map(param -> "-" + param.shortCode).collect(joining(", ")));
       System.out.println("");
       printHelp(requiredOperations, operations);
       System.exit(1);
