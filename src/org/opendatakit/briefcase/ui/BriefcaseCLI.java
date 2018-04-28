50840b6bd11765a4fc1a8eaea0acbbe057a422f6
Guille <ggalmazor@gmail.com>
2018-04-28 14:40:03 +0200

    Default to the new export on CLI

--- src/org/opendatakit/briefcase/ui/BriefcaseCLI.java
+++ src/org/opendatakit/briefcase/ui/BriefcaseCLI.java
@@ -17,7 +17,7 @@
 
 package org.opendatakit.briefcase.ui;
 
-import static org.opendatakit.briefcase.operations.Export.oldExport;
+import static org.opendatakit.briefcase.operations.Export.export;
 import static org.opendatakit.briefcase.operations.ImportFromODK.importODK;
 import static org.opendatakit.briefcase.operations.PullFormFromAggregate.pullFormFromAggregate;
 
@@ -307,7 +307,7 @@ public class BriefcaseCLI {
         pullFormFromAggregate(storageDir, formid, username, password, server);
 
       if (exportPath != null)
-        oldExport(
+        export(
             storageDir,
             formid,
             Paths.get(exportPath),
