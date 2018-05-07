439cd17f9f1b108f8d6338663a9b7f339c0e870a
Guille <ggalmazor@gmail.com>
2018-05-07 16:04:05 +0200

    Remove ignored test - These will become obsolete when all export events are migrated to ExportEvent instances

--- test/java/org/opendatakit/briefcase/ui/export/components/ExportFormsTableUnitTest.java
+++ test/java/org/opendatakit/briefcase/ui/export/components/ExportFormsTableUnitTest.java
@@ -16,23 +16,13 @@
 package org.opendatakit.briefcase.ui.export.components;
 
 import static org.junit.Assert.assertThat;
-import static org.opendatakit.briefcase.model.FormStatusBuilder.buildFormStatus;
 import static org.opendatakit.briefcase.model.FormStatusBuilder.buildFormStatusList;
 
-import java.util.Collections;
 import java.util.HashMap;
-import org.bushe.swing.event.EventBus;
 import org.hamcrest.Matchers;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.opendatakit.briefcase.export.ExportConfiguration;
 import org.opendatakit.briefcase.export.ExportForms;
-import org.opendatakit.briefcase.model.BriefcaseFormDefinition;
-import org.opendatakit.briefcase.model.ExportFailedEvent;
-import org.opendatakit.briefcase.model.ExportProgressEvent;
-import org.opendatakit.briefcase.model.ExportSucceededEvent;
-import org.opendatakit.briefcase.model.ExportSucceededWithErrorsEvent;
-import org.opendatakit.briefcase.model.FormStatus;
 
 public class ExportFormsTableUnitTest {
   @Test
@@ -58,26 +48,6 @@ public class ExportFormsTableUnitTest {
     formsTable.clearAll();
 
     assertThat(forms.noneSelected(), Matchers.is(true));
-  }
-
-  @Test
-  @Ignore
-  public void appends_to_a_forms_status_history_when_export_events_are_sent() {
-    FormStatus theForm = buildFormStatus(1);
-    ExportForms forms = new ExportForms(Collections.singletonList(theForm), ExportConfiguration.empty(), new HashMap<>(), new HashMap<>(), new HashMap<>());
-    TestExportFormsTableViewModel viewModel = new TestExportFormsTableViewModel(forms);
-    new ExportFormsTable(forms, new TestExportFormsTableView(viewModel), viewModel);
-
-    // TODO Event publishing happens asynchronously. We have to work this test a little more to stop ignoring it
-    EventBus.publish(new ExportProgressEvent("some progress", (BriefcaseFormDefinition) theForm.getFormDefinition()));
-    EventBus.publish(new ExportFailedEvent((BriefcaseFormDefinition) theForm.getFormDefinition()));
-    EventBus.publish(new ExportSucceededEvent((BriefcaseFormDefinition) theForm.getFormDefinition()));
-    EventBus.publish(new ExportSucceededWithErrorsEvent((BriefcaseFormDefinition) theForm.getFormDefinition()));
-
-    assertThat(theForm.getStatusHistory(), Matchers.containsString("some progress"));
-    assertThat(theForm.getStatusHistory(), Matchers.containsString("Failed."));
-    assertThat(theForm.getStatusHistory(), Matchers.containsString("Succeeded."));
-    assertThat(theForm.getStatusHistory(), Matchers.containsString("Succeeded, but with errors."));
   }
 
   private class TestExportFormsTableView extends ExportFormsTableView {
