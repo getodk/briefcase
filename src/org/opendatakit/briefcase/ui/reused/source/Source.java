2fab93d9a9f94468a801ab0457f60cf5a703319f
Guille <ggalmazor@gmail.com>
2018-05-15 16:00:14 +0200

    Implement a new Pull tab - We use IntelliJ's GUI Designer - We add a new tab to use it and compare results

---
+++ src/org/opendatakit/briefcase/ui/reused/source/Source.java
@@ -0,0 +1,293 @@
+/*
+ * Copyright (C) 2018 Nafundi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.opendatakit.briefcase.ui.reused.source;
+
+import static org.opendatakit.briefcase.model.FormStatus.TransferType.GATHER;
+import static org.opendatakit.briefcase.ui.StorageLocation.isUnderBriefcaseFolder;
+
+import java.awt.Container;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import org.opendatakit.briefcase.model.BriefcasePreferences;
+import org.opendatakit.briefcase.model.FormStatus;
+import org.opendatakit.briefcase.model.TerminationFuture;
+import org.opendatakit.briefcase.reused.BriefcaseException;
+import org.opendatakit.briefcase.reused.RemoteServer;
+import org.opendatakit.briefcase.reused.http.Http;
+import org.opendatakit.briefcase.ui.reused.FileChooser;
+import org.opendatakit.briefcase.util.FileSystemUtils;
+import org.opendatakit.briefcase.util.TransferAction;
+
+/**
+ * This interface represents a source (for lack of a better name) for pulling
+ * or pushing forms in the context of Briefcase's UI.
+ * <p>
+ * Currently, Briefcase supports:
+ * <ul>
+ * <li>Pulling from:<ul>
+ * <li>An <b>Aggregate</b> server</li>
+ * <li>A <b>Custom Directory</b> pointing to the ODK Collect storage</li>
+ * </ul></li>
+ * <li>Pushing to:<ul>
+ * <li>An <b>Aggregate</b> server</li>
+ * </ul></li>
+ * </ul>
+ * <p>
+ * This interface follows the "sealed trait" pattern from Scala. This means:
+ * <ul>
+ * <li>No direct usage of subtypes of Source should be found outside this file.</li>
+ * <li>Since this is Java, extracting subtypes to individual files is permitted but then,
+ * the same rule should be applied to the namespace.</li>
+ * <li>To obtain a new instance of Source, you need to use the static factories
+ * and you can't cast the result to any subtype of Source.</li>
+ * <li>No <code>instanceof</code> against any subtype is allowed.</li>
+ * </ul>
+ * <p>
+ * Notes on the implementation:
+ * <ul>
+ * <li>The fact that the method {@link Source#push} is only implemented by {@link Source.Aggregate}
+ * breaks Liskov's substitution principle, which hints about a flaw in the design of this
+ * type hierarchy.</li>
+ * <li>This interface should probably be segregated into two different PullSource and PushTarget
+ * interfaces which, not only will improve naming, but also will prevent breaking Liskov.</li>
+ * </ul>
+ *
+ * @param <T> the value type each Source produces when configured
+ */
+public interface Source<T> {
+  /**
+   * Factory of {@link Source} instances that deal with remote Aggregate servers.
+   *
+   * @param consumer {@link Consumer} that would be applied the {@link RemoteServer}
+   *                 instance configured by the user
+   */
+  static Source<RemoteServer> aggregate(Http http, Consumer<Source> consumer) {
+    return new Source.Aggregate(http, consumer);
+  }
+
+  /**
+   * Factory of {@link Source} instances that deal with custom filesystem directories
+   * pointing to a ODK Collect storage directory.
+   *
+   * @param consumer {@link Consumer} that would be applied the {@link Path} once
+   *                 the user selects it
+   */
+  static Source<Path> customDir(Consumer<Source> consumer) {
+    return new Source.CustomDir(consumer);
+  }
+
+  /**
+   * Launches whatever means this {@link Source} requires to let the user configure
+   * its value.
+   *
+   * @param container {@link Container} object that could be used to pop dialogs up
+   */
+  // TODO Better replace this with an abstraction that deals with Briefcase dialogs
+  void onSelect(Container container);
+
+  /**
+   * Sets the value of this source.
+   */
+  void set(T t);
+
+  /**
+   * Tests the given object <code>o</code> to check if it's compatible with the type
+   * of values this {@link Source} is capable of storing.
+   *
+   * @return true if it is compatible, false otherwise
+   */
+  boolean accepts(Object o);
+
+  /**
+   * Returns the list of forms that this configured {@link Source} has access to.
+   */
+  List<FormStatus> getFormList();
+
+  /**
+   * Stores the value of this {@link Source} in the given {@link BriefcasePreferences}.
+   *
+   * @param storePasswords {@link Boolean} telling this source whether to store
+   *                       passwords or not
+   */
+  void storePreferences(BriefcasePreferences prefs, boolean storePasswords);
+
+  /**
+   * Pulls forms to the Briefcase Storage Directory from this configured {@link Source}.
+   *
+   * @param forms             {@link List} of forms to be pulled
+   * @param terminationFuture object that to make the operation cancellable
+   */
+  void pull(List<FormStatus> forms, TerminationFuture terminationFuture);
+
+  /**
+   * Pushes forms to this configured {@link Source}.
+   *
+   * @param forms             {@link List} of forms to be pulled
+   * @param terminationFuture object that to make the operation cancellable
+   */
+  void push(List<FormStatus> forms, TerminationFuture terminationFuture);
+
+  /**
+   * Returns a textual description of this {@link Source}.
+   */
+  String getDescription();
+
+  class Aggregate implements Source<RemoteServer> {
+    private final Http http;
+    private final Consumer<Source> consumer;
+    private RemoteServer server;
+
+    Aggregate(Http http, Consumer<Source> consumer) {
+      this.http = http;
+      this.consumer = consumer;
+    }
+
+    @Override
+    public void onSelect(Container ignored) {
+      RemoteServerDialog dialog = RemoteServerDialog.empty(http);
+      dialog.onConnect(this::set);
+      dialog.getForm().setVisible(true);
+    }
+
+    @Override
+    public void set(RemoteServer server) {
+      this.server = server;
+      consumer.accept(this);
+    }
+
+    @Override
+    public boolean accepts(Object o) {
+      return o instanceof RemoteServer;
+    }
+
+    @Override
+    public List<FormStatus> getFormList() {
+      return server.getFormsList(http).stream()
+          .map(rfd -> new FormStatus(GATHER, rfd))
+          .collect(Collectors.toList());
+    }
+
+    @Override
+    public void storePreferences(BriefcasePreferences prefs, boolean storePasswords) {
+      server.storePreferences(prefs, storePasswords);
+    }
+
+    @Override
+    public void pull(List<FormStatus> forms, TerminationFuture terminationFuture) {
+      try {
+        TransferAction.transferServerToBriefcase(server.asServerConnectionInfo(), terminationFuture, forms);
+      } catch (IOException e) {
+        throw new UncheckedIOException(e);
+      }
+    }
+
+    @Override
+    public void push(List<FormStatus> forms, TerminationFuture terminationFuture) {
+      try {
+        TransferAction.transferBriefcaseToServer(server.asServerConnectionInfo(), terminationFuture, forms, http, server);
+      } catch (IOException e) {
+        throw new UncheckedIOException(e);
+      }
+    }
+
+    @Override
+    public String getDescription() {
+      return server.getBaseUrl().toString();
+    }
+
+    @Override
+    public String toString() {
+      return "Aggregate";
+    }
+  }
+
+  class CustomDir implements Source<Path> {
+    private final Consumer<Source> consumer;
+    private Path path;
+
+    CustomDir(Consumer<Source> consumer) {
+      this.consumer = consumer;
+    }
+
+    static boolean isValidCustomDir(Path f) {
+      return Files.exists(f) && Files.isDirectory(f) && !isUnderBriefcaseFolder(f.toFile()) && Files.exists(f.resolve("forms")) && Files.isDirectory(f.resolve("forms"));
+    }
+
+    @Override
+    public void onSelect(Container container) {
+      FileChooser
+          .directory(container, Optional.empty(), f -> isValidCustomDir(f.toPath()), "ODK Collect Folders")
+          .choose()
+          // TODO Changing the FileChooser to handle Paths instead of Files would improve this code and it's also coherent with the modernization (use NIO2 API) of this basecode
+          .ifPresent(file -> set(file.toPath()));
+    }
+
+    @Override
+    public void set(Path path) {
+      this.path = path;
+      consumer.accept(this);
+    }
+
+    @Override
+    public boolean accepts(Object o) {
+      return o instanceof Path;
+    }
+
+    @Override
+    public List<FormStatus> getFormList() {
+      return FileSystemUtils.getODKFormList(path.toFile()).stream()
+          .map(formDef -> new FormStatus(GATHER, formDef))
+          .collect(Collectors.toList());
+    }
+
+    @Override
+    public void storePreferences(BriefcasePreferences prefs, boolean storePasswords) {
+      // No prefs to store
+    }
+
+    @Override
+    public void pull(List<FormStatus> forms, TerminationFuture terminationFuture) {
+      try {
+        TransferAction.transferODKToBriefcase(path.toFile(), terminationFuture, forms);
+      } catch (IOException e) {
+        throw new UncheckedIOException(e);
+      }
+    }
+
+    @Override
+    public void push(List<FormStatus> forms, TerminationFuture terminationFuture) {
+      throw new BriefcaseException("Can't push to a Custom Directory");
+    }
+
+    @Override
+    public String getDescription() {
+      return path.toString();
+    }
+
+    @Override
+    public String toString() {
+      return "Custom Directory";
+    }
+  }
+}
+
