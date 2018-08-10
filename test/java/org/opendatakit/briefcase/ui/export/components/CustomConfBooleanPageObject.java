dd2d74cdd70e969c59fffe4a23b3aee19935274d
Guille <ggalmazor@gmail.com>
2018-08-10 11:38:43 +0200

    New UI component for custom export conf boolean fields - The new component effectively implements a ternary checkbox with an UNDETERMINED field - The UNDETERMINED status applied to this context is the INHERIT option - The component uses inlined radio buttons to reduce UI space use - There's a test class that goes over the behavior of the new component

---
+++ test/java/org/opendatakit/briefcase/ui/export/components/CustomConfBooleanPageObject.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2018 Nafundi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.opendatakit.briefcase.ui.export.components;
+
+import static org.assertj.swing.edt.GuiActionRunner.execute;
+
+import java.awt.event.ActionEvent;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.function.Consumer;
+import javax.swing.JFrame;
+import javax.swing.JRadioButton;
+import org.assertj.swing.core.Robot;
+import org.assertj.swing.fixture.FrameFixture;
+
+class CustomConfBooleanPageObject {
+  private final CustomConfBoolean component;
+  private final FrameFixture fixture;
+
+  private CustomConfBooleanPageObject(CustomConfBoolean component, FrameFixture fixture) {
+    this.component = component;
+    this.fixture = fixture;
+  }
+
+  static CustomConfBooleanPageObject setUp(Robot robot, Optional<CustomConfBoolean.Value> initialValue) {
+    CustomConfBoolean component = execute(() -> new CustomConfBoolean(initialValue));
+    JFrame frame = execute(() -> {
+      JFrame f = new JFrame();
+      f.add(component.$$$getRootComponent$$$());
+      return f;
+    });
+    FrameFixture fixture = new FrameFixture(robot, frame);
+    return new CustomConfBooleanPageObject(component, fixture);
+  }
+
+  void show() {
+    fixture.show();
+  }
+
+  public void onChange(Consumer<CustomConfBoolean.Value> callback) {
+    component.onChange(callback);
+  }
+
+  JRadioButton inherit() {
+    return component.inherit;
+  }
+
+  JRadioButton yes() {
+    return component.yes;
+  }
+
+  JRadioButton no() {
+    return component.no;
+  }
+
+  void set(CustomConfBoolean.Value value) {
+    execute(() -> {
+      component.set(value);
+      component.setInternal(value);
+    });
+  }
+
+  private JRadioButton getRadioButton(CustomConfBoolean.Value value) {
+    switch (value) {
+      case INHERIT:
+        return component.inherit;
+      case YES:
+        return component.yes;
+      case NO:
+        return component.no;
+      default:
+        throw new IllegalArgumentException("Unknown radio button for " + value);
+    }
+  }
+}
---
+++ test/java/org/opendatakit/briefcase/ui/export/components/CustomConfBooleanTest.java
@@ -0,0 +1,97 @@
+package org.opendatakit.briefcase.ui.export.components;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertThat;
+import static org.opendatakit.briefcase.matchers.SwingMatchers.selected;
+import static org.opendatakit.briefcase.ui.export.components.CustomConfBoolean.Value.INHERIT;
+import static org.opendatakit.briefcase.ui.export.components.CustomConfBoolean.Value.NO;
+import static org.opendatakit.briefcase.ui.export.components.CustomConfBoolean.Value.YES;
+
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.junit.Test;
+
+public class CustomConfBooleanTest extends AssertJSwingJUnitTestCase {
+  private CustomConfBooleanPageObject component;
+
+  @Override
+  protected void onSetUp() {
+    // component creation is made on each test to allow different scenarios
+  }
+
+  @Test
+  public void inherit_is_selected_if_no_inital_value_is_given() {
+    component = CustomConfBooleanPageObject.setUp(robot(), Optional.empty());
+    component.show();
+
+    assertThat(component.inherit(), is(selected()));
+    assertThat(component.yes(), is(not(selected())));
+    assertThat(component.no(), is(not(selected())));
+  }
+
+  @Test
+  public void it_can_have_an_initial_value_different_than_inherit() {
+    component = CustomConfBooleanPageObject.setUp(robot(), Optional.of(YES));
+    component.show();
+
+    assertThat(component.inherit(), is(not(selected())));
+    assertThat(component.yes(), is(selected()));
+    assertThat(component.no(), is(not(selected())));
+  }
+
+  @Test
+  public void selecting_a_different_option_unselects_others() {
+    // This test verifies that all radio buttons belong to the same ButtonGroup
+    component = CustomConfBooleanPageObject.setUp(robot(), Optional.of(YES));
+    component.show();
+
+    component.set(NO);
+
+    assertThat(component.inherit(), is(not(selected())));
+    assertThat(component.yes(), is(not(selected())));
+    assertThat(component.no(), is(selected()));
+  }
+
+  @Test
+  public void lets_third_parties_subscribe_to_change_events() {
+    Wrapper<CustomConfBoolean.Value> lastValue = new Wrapper<>(INHERIT);
+    component = CustomConfBooleanPageObject.setUp(robot(), Optional.of(lastValue.get()));
+    component.onChange(lastValue::set);
+    component.show();
+
+    component.set(YES);
+
+    assertThat(lastValue.get(), is(YES));
+  }
+
+  @Test
+  public void avoids_sending_duplicate_events() {
+    final AtomicInteger changeCounts = new AtomicInteger(0);
+    component = CustomConfBooleanPageObject.setUp(robot(), Optional.of(NO));
+    component.onChange(__ -> changeCounts.incrementAndGet());
+    component.show();
+
+    component.set(YES);
+    component.set(YES);
+
+    assertThat(changeCounts.get(), is(1));
+  }
+
+  class Wrapper<T> {
+    public T t;
+
+    Wrapper(T t) {
+      this.t = t;
+    }
+
+    public T get() {
+      return t;
+    }
+
+    public void set(T newT) {
+      t = newT;
+    }
+  }
+}
