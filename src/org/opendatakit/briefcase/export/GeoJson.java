bc2e5a38062c2cf6ce62a6b6969d486ab1a478f6
Guille <ggalmazor@gmail.com>
2018-10-15 19:28:21 +0200

    Improve GeoJson.toFeatures() semantics and design - Simplify code to be less streamy and more easy-to-understand-imperative   - Avoid using Optional for exception management (in the case the type and serialized data is incompatible) - Extract to static members the point component and point string separators - Extract a generic method to parse bound and unbounded Doubles

--- src/org/opendatakit/briefcase/export/GeoJson.java
+++ src/org/opendatakit/briefcase/export/GeoJson.java
@@ -16,6 +16,7 @@
 
 package org.opendatakit.briefcase.export;
 
+import static java.lang.Math.abs;
 import static java.util.Collections.emptyList;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Stream.empty;
@@ -37,19 +38,35 @@ import org.geojson.Polygon;
 import org.javarosa.core.model.DataType;
 import org.opendatakit.briefcase.reused.OptionalProduct;
 import org.opendatakit.briefcase.reused.UncheckedFiles;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class GeoJson {
+  private static final Logger log = LoggerFactory.getLogger(GeoJson.class);
+  private static final String POINT_COMPONENT_SEPARATOR = " ";
+  private static final String POINT_STRING_SEPARATOR = ";";
 
   static Stream<Feature> toFeatures(Model model, Submission submission) {
     String instanceId = submission.getInstanceId(false);
     return model.getSpatialFields().stream().map(field -> {
+      // Get the value on the submission
       Optional<String> maybeValue = submission.findElement(field.getName()).flatMap(XmlElement::maybeValue);
-      return maybeValue.map(value -> {
-        List<LngLatAlt> lngLatAlts = maybeValue.map(GeoJson::toLngLatAlts).orElse(emptyList());
-        return toGeoJsonObject(field, lngLatAlts)
-            .map(geoJsonObject -> validFeature(field, instanceId, geoJsonObject))
-            .orElse(invalidFeature(field, instanceId));
-      }).orElse(emptyFeature(field, instanceId));
+      if (!maybeValue.isPresent())
+        return emptyFeature(field, instanceId);
+
+      // Transform the point string to a list of LngLatAlts
+      List<LngLatAlt> lngLatAlts = maybeValue.map(GeoJson::toLngLatAlts).orElse(emptyList());
+
+      // TODO This would be much better with a Try<GeoJsonObject>
+      try {
+        // Build the corresponding GeoJSON object and return a valid Feature
+        GeoJsonObject geoJsonObject = toGeoJsonObject(field, lngLatAlts);
+        return validFeature(field, instanceId, geoJsonObject);
+      } catch (IllegalArgumentException e) {
+        // Log the combination of field type and serialized data and return an invalid Feature
+        log.debug("Illegal combination of field type {} and spatial data {}", field.getDataType().name(), maybeValue.get());
+        return invalidFeature(field, instanceId);
+      }
     });
   }
 
@@ -65,18 +82,18 @@ class GeoJson {
   }
 
   private static Feature validFeature(Model field, String instanceId, GeoJsonObject geoJsonObject) {
-    return toFeature(field, instanceId, Optional.of(geoJsonObject), false, true);
+    return feature(field, instanceId, Optional.of(geoJsonObject), false, true);
   }
 
   private static Feature emptyFeature(Model field, String instanceId) {
-    return toFeature(field, instanceId, Optional.empty(), true, true);
+    return feature(field, instanceId, Optional.empty(), true, true);
   }
 
   private static Feature invalidFeature(Model field, String instanceId) {
-    return toFeature(field, instanceId, Optional.empty(), false, false);
+    return feature(field, instanceId, Optional.empty(), false, false);
   }
 
-  private static Feature toFeature(Model field, String instanceId, Optional<GeoJsonObject> geoJsonObject, boolean empty, boolean valid) {
+  private static Feature feature(Model field, String instanceId, Optional<GeoJsonObject> geoJsonObject, boolean empty, boolean valid) {
     Feature feature = new Feature();
     feature.setGeometry(geoJsonObject.orElse(null));
     feature.setProperty("key", instanceId);
@@ -86,50 +103,44 @@ class GeoJson {
     return feature;
   }
 
-  static List<LngLatAlt> toLngLatAlts(String value) {
-    Stream<String> split = Stream.of(value.split(";"));
-    Stream<LngLatAlt> optionalStream = split.flatMap(s -> GeoJson.getLngLatAlt(s).map(Stream::of).orElse(empty()));
-    return optionalStream.collect(toList());
+  private static List<LngLatAlt> toLngLatAlts(String value) {
+    return Stream.of(value.split(POINT_STRING_SEPARATOR))
+        .flatMap(s -> GeoJson.parseLngLatAlt(s).map(Stream::of).orElse(empty()))
+        .collect(toList());
   }
 
-  private static Optional<GeoJsonObject> toGeoJsonObject(Model field, List<LngLatAlt> lngLatAlts) {
+  private static GeoJsonObject toGeoJsonObject(Model field, List<LngLatAlt> lngLatAlts) {
     if (field.getDataType() == DataType.GEOPOINT && lngLatAlts.size() == 1)
-      return Optional.of(new Point(lngLatAlts.get(0)));
+      return new Point(lngLatAlts.get(0));
 
     if (field.getDataType() == DataType.GEOTRACE && lngLatAlts.size() >= 2)
-      return Optional.of(new LineString(lngLatAlts.toArray(new LngLatAlt[0])));
+      return new LineString(lngLatAlts.toArray(new LngLatAlt[0]));
 
     if (field.getDataType() == DataType.GEOSHAPE && lngLatAlts.size() >= 4 && lngLatAlts.get(0).equals(lngLatAlts.get(lngLatAlts.size() - 1)))
-      return Optional.of(new Polygon(lngLatAlts.toArray(new LngLatAlt[0])));
+      return new Polygon(lngLatAlts.toArray(new LngLatAlt[0]));
 
-    return Optional.empty();
+    throw new IllegalArgumentException();
   }
 
-  private static Optional<LngLatAlt> getLngLatAlt(String geoPoint) {
-    String[] fields = geoPoint.split(" ");
+  private static Optional<LngLatAlt> parseLngLatAlt(String geoPoint) {
+    String[] fields = geoPoint.split(POINT_COMPONENT_SEPARATOR);
     if (fields.length < 2 || fields.length > 4)
       return Optional.empty();
-    Optional<Double> altitude = readAltitude(fields);
+    Optional<Double> altitude = fields.length > 2 ? maybeParseDouble(fields[2]) : Optional.empty();
     return OptionalProduct.all(
-        readLongitude(fields[1]),
-        readLatitude(fields[0])
+        maybeParseDouble(fields[1], 180),
+        maybeParseDouble(fields[0], 90)
     ).map((lon, lat) -> altitude
         .map(alt -> new LngLatAlt(lon, lat, alt))
         .orElse(new LngLatAlt(lon, lat)));
   }
 
-  private static Optional<Double> readLatitude(String field) {
-    return Optional.ofNullable(field).filter(s -> !s.isEmpty()).map(Double::parseDouble).filter(lat -> lat >= -90 && lat <= 90);
+  private static Optional<Double> maybeParseDouble(String value) {
+    return Optional.ofNullable(value).filter(s -> !s.isEmpty()).map(Double::parseDouble);
   }
 
-  private static Optional<Double> readLongitude(String field) {
-    return Optional.ofNullable(field).filter(s -> !s.isEmpty()).map(Double::parseDouble).filter(lng -> lng >= -180 && lng <= 180);
-  }
-
-  private static Optional<Double> readAltitude(String[] fields) {
-    return fields.length > 2
-        ? Optional.ofNullable(fields[2]).map(Double::parseDouble)
-        : Optional.empty();
+  private static Optional<Double> maybeParseDouble(String value, int absBound) {
+    return Optional.ofNullable(value).filter(s -> !s.isEmpty()).map(Double::parseDouble).filter(lat -> abs(lat) <= absBound);
   }
 
 }
