011527268b220d983a9c4056be55723d82d96a6d
Guille <ggalmazor@gmail.com>
2018-12-05 08:53:18 +0100

    Give some (hopefully right) semantics to the char replacements

--- src/org/opendatakit/briefcase/util/StringUtils.java
+++ src/org/opendatakit/briefcase/util/StringUtils.java
@@ -19,20 +19,26 @@ package org.opendatakit.briefcase.util;
 
 public class StringUtils {
 
+  private static final String INPUTS_COMPOSED_ONLY_BY_DOTS_AND_SLASHES = "^[./]+$";
+  private static final String TRAILING_DOTS_AND_SPACES = "[. ]+$";
+  private static final String WINDOWSY_RESERVED_NAMES = "^(con|prn|aux|nul|com[0-9]|lpt[0-9]|CON|PRN|AUX|NUL|COM[0-9]|LPT[0-9])(\\..*)?";
+  private static final String CONTROL_CHARS = "[\\x00-\\x1f\\x80-\\x9f]";
+  private static final String SOME_PUNCTUATION_CHARS = "[/?<>\\\\*|\":]";
+
   public static boolean nullOrEmpty(String text) {
     return text == null || text.trim().length() == 0;
   }
 
-  public static String stripIllegalChars(String text) {
-    if (text == null)
+  public static String stripIllegalChars(String input) {
+    if (input == null)
       return null;
-    return text
-        .replaceAll("^[./]+$", "")
-        .replaceAll("[. ]+$", "")
-        // Apparently, the library Central's using doesn't trim heading spaces
+    return input
+        .replaceAll(INPUTS_COMPOSED_ONLY_BY_DOTS_AND_SLASHES, "")
+        .replaceAll(TRAILING_DOTS_AND_SPACES, "")
+        // Apparently, the library Central's using doesn't trim heading dots and spaces
         // .replaceAll("^[. ]+", "")
-        .replaceAll("^(con|prn|aux|nul|com[0-9]|lpt[0-9]|CON|PRN|AUX|NUL|COM[0-9]|LPT[0-9])(\\..*)?", "")
-        .replaceAll("[\\x00-\\x1f\\x80-\\x9f]", "")
-        .replaceAll("[/?<>\\\\*|\":]", "");
+        .replaceAll(WINDOWSY_RESERVED_NAMES, "")
+        .replaceAll(CONTROL_CHARS, "")
+        .replaceAll(SOME_PUNCTUATION_CHARS, "");
   }
 }
