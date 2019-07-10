20e86566b489cf85875f98095675d360146526c4
Guille <ggalmazor@gmail.com>
2019-07-10 18:05:14 +0200

    Duplicate push to aggregate CLI op as a base for the Central version

---
+++ src/org/opendatakit/briefcase/operations/PushFormToCentral.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright (C) 2018 Nafundi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.opendatakit.briefcase.operations;
+
+import static org.opendatakit.briefcase.operations.Common.CREDENTIALS_PASSWORD;
+import static org.opendatakit.briefcase.operations.Common.CREDENTIALS_USERNAME;
+import static org.opendatakit.briefcase.operations.Common.FORM_ID;
+import static org.opendatakit.briefcase.operations.Common.MAX_HTTP_CONNECTIONS;
+import static org.opendatakit.briefcase.operations.Common.SERVER_URL;
+import static org.opendatakit.briefcase.operations.Common.STORAGE_DIR;
+import static org.opendatakit.briefcase.reused.http.Http.DEFAULT_HTTP_CONNECTIONS;
+
+import java.net.URL;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Optional;
+import org.opendatakit.briefcase.model.BriefcasePreferences;
+import org.opendatakit.briefcase.model.FormStatus;
+import org.opendatakit.briefcase.model.FormStatusEvent;
+import org.opendatakit.briefcase.push.aggregate.PushToAggregate;
+import org.opendatakit.briefcase.reused.BriefcaseException;
+import org.opendatakit.briefcase.reused.Optionals;
+import org.opendatakit.briefcase.reused.http.CommonsHttp;
+import org.opendatakit.briefcase.reused.http.Credentials;
+import org.opendatakit.briefcase.reused.http.Http;
+import org.opendatakit.briefcase.reused.http.response.Response;
+import org.opendatakit.briefcase.reused.job.JobsRunner;
+import org.opendatakit.briefcase.reused.transfer.AggregateServer;
+import org.opendatakit.briefcase.transfer.TransferForms;
+import org.opendatakit.briefcase.util.FormCache;
+import org.opendatakit.common.cli.Operation;
+import org.opendatakit.common.cli.Param;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PushFormToCentral {
+  private static final Logger log = LoggerFactory.getLogger(PushFormToCentral.class);
+  private static final Param<Void> PUSH_AGGREGATE = Param.flag("psha", "push_aggregate", "Push form to an Aggregate instance");
+  private static final Param<Void> FORCE_SEND_BLANK = Param.flag("fsb", "force_send_blank", "Force sending the blank form to the Aggregate instance");
+
+  public static Operation OPERATION = Operation.of(
+      PUSH_AGGREGATE,
+      args -> pushFormToAggregate(
+          args.get(STORAGE_DIR),
+          args.get(FORM_ID),
+          args.get(CREDENTIALS_USERNAME),
+          args.get(CREDENTIALS_PASSWORD),
+          args.get(SERVER_URL),
+          args.has(FORCE_SEND_BLANK),
+          args.getOptional(MAX_HTTP_CONNECTIONS)
+      ),
+      Arrays.asList(STORAGE_DIR, FORM_ID, CREDENTIALS_USERNAME, CREDENTIALS_PASSWORD, SERVER_URL),
+      Arrays.asList(FORCE_SEND_BLANK, MAX_HTTP_CONNECTIONS)
+  );
+
+  private static void pushFormToAggregate(String storageDir, String formid, String username, String password, URL server, boolean forceSendBlank, Optional<Integer> maybeMaxConnections) {
+    CliEventsCompanion.attach(log);
+    Path briefcaseDir = Common.getOrCreateBriefcaseDir(storageDir);
+    FormCache formCache = FormCache.from(briefcaseDir);
+    formCache.update();
+    BriefcasePreferences appPreferences = BriefcasePreferences.appScoped();
+
+    int maxHttpConnections = Optionals.race(
+        maybeMaxConnections,
+        appPreferences.getMaxHttpConnections()
+    ).orElse(DEFAULT_HTTP_CONNECTIONS);
+    Http http = appPreferences.getHttpProxy()
+        .map(host -> CommonsHttp.of(maxHttpConnections, host))
+        .orElseGet(() -> CommonsHttp.of(maxHttpConnections));
+
+    AggregateServer aggregateServer = AggregateServer.authenticated(server, new Credentials(username, password));
+
+    Response response = http.execute(aggregateServer.getPushFormPreflightRequest());
+    if (!response.isSuccess()) {
+      System.err.println(response.isRedirection()
+          ? "Error connecting to Aggregate: Redirection detected"
+          : response.isUnauthorized()
+          ? "Error connecting to Aggregate: Wrong credentials"
+          : response.isNotFound()
+          ? "Error connecting to Aggregate: Aggregate not found"
+          : "Error connecting to Aggregate");
+      return;
+    }
+    Optional<FormStatus> maybeFormStatus = formCache.getForms().stream()
+        .filter(form -> form.getFormId().equals(formid))
+        .map(FormStatus::new)
+        .findFirst();
+
+    FormStatus form = maybeFormStatus.orElseThrow(() -> new BriefcaseException("Form " + formid + " not found"));
+    TransferForms forms = TransferForms.of(form);
+    forms.selectAll();
+
+    PushToAggregate pushOp = new PushToAggregate(http, aggregateServer, briefcaseDir, forceSendBlank, PushFormToCentral::onEvent);
+    JobsRunner.launchAsync(forms.map(pushOp::push), PushFormToCentral::onError).waitForCompletion();
+    System.out.println();
+    System.out.println("All operations completed");
+    System.out.println();
+  }
+
+  private static void onEvent(FormStatusEvent event) {
+    System.out.println(event.getStatus().getFormName() + " - " + event.getStatusString());
+    // The PullTracker already logs normal events
+  }
+
+  private static void onError(Throwable e) {
+    System.err.println("Error pushing a form: " + e.getMessage() + " (see the logs for more info)");
+    log.error("Error pushing a form", e);
+  }
+
+}
