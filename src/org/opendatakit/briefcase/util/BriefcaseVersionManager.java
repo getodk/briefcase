dbf51c91a2ce0bb44a2d29e3593c553bcc443a31
Guille <ggalmazor@gmail.com>
2019-08-01 12:33:33 +0200

    Add a class that knows how to get the latest available version

---
+++ src/org/opendatakit/briefcase/util/BriefcaseVersionManager.java
@@ -0,0 +1,37 @@
+package org.opendatakit.briefcase.util;
+
+import static org.opendatakit.briefcase.reused.http.RequestBuilder.url;
+
+import java.net.URL;
+import org.opendatakit.briefcase.reused.http.Http;
+import org.opendatakit.briefcase.reused.http.RequestBuilder;
+
+public class BriefcaseVersionManager {
+  private final Http http;
+  private final String current;
+
+  public BriefcaseVersionManager(Http http, String current) {
+    this.http = http;
+    this.current = current.contains("-") ? current.substring(0, current.indexOf("-")) : current;
+  }
+
+  public boolean isUpToDate() {
+    return getCurrent().equals(getLatest());
+  }
+
+  public String getLatest() {
+    return http.execute(RequestBuilder.get("https://api.github.com/repos/opendatakit/briefcase/releases/latest")
+        .asJsonMap()
+        .withResponseMapper(node -> (String) node.get("tag_name"))
+        .build())
+        .get();
+  }
+
+  public String getCurrent() {
+    return current;
+  }
+
+  public static URL getLatestUrl() {
+    return url("https://github.com/opendatakit/briefcase/releases/latest");
+  }
+}
---
+++ test/java/org/opendatakit/briefcase/util/BriefcaseVersionManagerTest.java
@@ -0,0 +1,60 @@
+package org.opendatakit.briefcase.util;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.opendatakit.briefcase.reused.http.RequestBuilder.get;
+import static org.opendatakit.briefcase.reused.http.RequestBuilder.url;
+import static org.opendatakit.briefcase.util.BriefcaseVersionManager.getLatestUrl;
+
+import org.junit.Test;
+import org.opendatakit.briefcase.reused.http.FakeHttp;
+import org.opendatakit.briefcase.reused.http.response.ResponseHelpers;
+
+public class BriefcaseVersionManagerTest {
+  @Test
+  public void knows_the_url_to_get_the_latest_version() {
+    assertThat(getLatestUrl(), is(url("https://github.com/opendatakit/briefcase/releases/latest")));
+  }
+
+  @Test
+  public void knows_to_get_the_latest_available_version() {
+    FakeHttp http = new FakeHttp();
+
+    http.stub(
+        get(url("https://api.github.com/repos/opendatakit/briefcase/releases/latest")).build(),
+        ResponseHelpers.ok("{\"tag_name\":\"v2.0.0\"}")
+    );
+
+    BriefcaseVersionManager versionManager = new BriefcaseVersionManager(http, "v1.16.0");
+
+    assertThat(versionManager.getLatest(), is("v2.0.0"));
+  }
+
+  @Test
+  public void knows_if_we_are_up_to_date() {
+    FakeHttp http = new FakeHttp();
+
+    http.stub(
+        get(url("https://api.github.com/repos/opendatakit/briefcase/releases/latest")).build(),
+        ResponseHelpers.ok("{\"tag_name\":\"v2.0.0\"}")
+    );
+
+    BriefcaseVersionManager versionManager = new BriefcaseVersionManager(http, "v2.0.0");
+
+    assertThat(versionManager.isUpToDate(), is(true));
+  }
+
+  @Test
+  public void knows_if_we_are_not_up_to_date() {
+    FakeHttp http = new FakeHttp();
+
+    http.stub(
+        get(url("https://api.github.com/repos/opendatakit/briefcase/releases/latest")).build(),
+        ResponseHelpers.ok("{\"tag_name\":\"v2.0.0\"}")
+    );
+
+    BriefcaseVersionManager versionManager = new BriefcaseVersionManager(http, "v1.6.0");
+
+    assertThat(versionManager.isUpToDate(), is(false));
+  }
+}
