0812054d286f59a44ef5b339020b6d62e199dfce
Guille <ggalmazor@gmail.com>
2019-08-22 13:39:47 +0200

    Create the first migration for form metadata tables and generate model

---
+++ src/main/resources/db/migration/V0.1__form_metadata.sql
@@ -0,0 +1,14 @@
+CREATE SEQUENCE form_metadata_id_seq;
+
+CREATE TABLE form_metadata (
+    id                            INTEGER NOT NULL PRIMARY KEY,
+    form_name                     VARCHAR(255),
+    form_id                       VARCHAR(255),
+    form_version                  VARCHAR(255),
+    form_dir                      VARCHAR(255),
+    form_filename                 VARCHAR(255),
+    cursor_type                   VARCHAR(20),
+    cursor_value                  CLOB,
+    last_exported_submission_date TIMESTAMP WITH TIME ZONE,
+    CHECK ((form_name IS NOT NULL) OR (form_id IS NOT NULL))
+);
