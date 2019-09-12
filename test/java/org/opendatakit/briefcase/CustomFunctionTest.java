6d0dd13d3f6f1458cd63ad3c1ffad41c20990709
Guillermo Gutiérrez <ggalmazor@gmail.com>
2019-09-12 11:02:21 +0200

    Provide a no-op fallback handler to avoid errors with custom functions

--- test/java/org/opendatakit/briefcase/CustomFunctionTest.java
+++ test/java/org/opendatakit/briefcase/CustomFunctionTest.java
@@ -1,26 +1,15 @@
 package org.opendatakit.briefcase;
 
-import static org.opendatakit.briefcase.reused.UncheckedFiles.createDirectories;
-import static org.opendatakit.briefcase.reused.UncheckedFiles.createTempDirectory;
+import static org.junit.Assert.assertThat;
 import static org.opendatakit.briefcase.reused.UncheckedFiles.toURI;
 
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Optional;
+import org.hamcrest.Matchers;
 import org.junit.Test;
-import org.opendatakit.briefcase.export.DateRange;
-import org.opendatakit.briefcase.export.ExportConfiguration;
-import org.opendatakit.briefcase.export.ExportToCsv;
 import org.opendatakit.briefcase.export.FormDefinition;
-import org.opendatakit.briefcase.model.BriefcaseFormDefinition;
-import org.opendatakit.briefcase.model.FormStatus;
-import org.opendatakit.briefcase.model.form.FormMetadata;
-import org.opendatakit.briefcase.model.form.InMemoryFormMetadataAdapter;
 import org.opendatakit.briefcase.pull.aggregate.PullFromAggregateIntegrationTest;
-import org.opendatakit.briefcase.reused.OverridableBoolean;
-import org.opendatakit.briefcase.util.BadFormDefinition;
 
 public class CustomFunctionTest {
   private static Path getPath(String fileName) {
@@ -31,37 +20,9 @@ public class CustomFunctionTest {
 
   @Test
   public void name() {
-    FormDefinition.from(getPath("custom-function-form.xml"));
-  }
-
-  @Test
-  public void full_regression_test() throws IOException, BadFormDefinition {
-    Path exportDir = createTempDirectory("briefcase-test-export-dir-");
-    Path sourceFormFile = getPath("custom-function-form.xml");
-    Path briefcaseDir = createTempDirectory("briefcase-test-storage-dir-");
-    Path formFile = briefcaseDir.resolve("Custom function form").resolve("Custom function form.xml");
-    createDirectories(formFile.getParent());
-    Files.copy(sourceFormFile, formFile);
-
-    FormMetadata formMetadata = FormMetadata.from(formFile);
-    InMemoryFormMetadataAdapter formMetadataPort = new InMemoryFormMetadataAdapter();
-    formMetadataPort.persist(formMetadata);
-    BriefcaseFormDefinition localFormDef = BriefcaseFormDefinition.resolveAgainstBriefcaseDefn(formFile.toFile(), false, briefcaseDir.toFile());
-    FormStatus formStatus = new FormStatus(localFormDef);
-
-    ExportConfiguration configuration = new ExportConfiguration(
-        Optional.<String>empty(),
-        Optional.of(exportDir),
-        Optional.<Path>empty(),
-        DateRange.empty(),
-        OverridableBoolean.FALSE,
-        OverridableBoolean.TRUE,
-        OverridableBoolean.TRUE,
-        OverridableBoolean.FALSE,
-        OverridableBoolean.FALSE,
-        OverridableBoolean.FALSE,
-        OverridableBoolean.FALSE
-    );
-    ExportToCsv.export(formMetadataPort, formMetadata, formStatus, FormDefinition.from(localFormDef), briefcaseDir, configuration);
+    FormDefinition formDefinition = FormDefinition.from(getPath("custom-function-form.xml"));
+    // Assert anything about the form definition object
+    // What's important is that we can get the form def object without errors
+    assertThat(formDefinition.getFormId(), Matchers.is("custom-function-form"));
   }
 }
