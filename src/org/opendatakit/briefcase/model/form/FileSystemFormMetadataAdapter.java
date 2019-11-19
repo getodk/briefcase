421ba89c10f0718cbd670f7c5f1b655efc23a8b2
Guille <ggalmazor@gmail.com>
2019-11-19 15:20:13 +0100

    Continue refactor.
    Remove unused arg

--- src/org/opendatakit/briefcase/model/form/FileSystemFormMetadataAdapter.java
+++ src/org/opendatakit/briefcase/model/form/FileSystemFormMetadataAdapter.java
@@ -53,7 +53,7 @@ public class FileSystemFormMetadataAdapter implements FormMetadataPort {
 
     // select XML files that look like forms by parsing them
     // and looking for key parts that all forms must have
-    Stream<Path> formFiles = candidateFormFiles.filter(path -> isAForm(path, XmlElement.from(path)));
+    Stream<Path> formFiles = candidateFormFiles.filter(path -> isAForm(path));
 
     // Parse existing metadata.json files or build new FormMetadata from form files
     // At this point, we collect the stream to avoid problems coming
@@ -78,7 +78,7 @@ public class FileSystemFormMetadataAdapter implements FormMetadataPort {
     return this;
   }
 
-  private boolean isAForm(Path path, XmlElement root) {
+  private boolean isAForm(Path path) {
     XmlElement foo = XmlElement.from(path);
     return foo.getName().equals("html")
         && foo.findElements("head", "title").size() == 1
