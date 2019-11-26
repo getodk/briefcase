384239b0120cbeff1fc652782fd57865ebbba8cb
Guille <ggalmazor@gmail.com>
2019-11-26 10:44:01 +0100

    Replace original pull op launcher with what we do in the UI
    Add the required .waitForCompletion() call in the JobsRunner object like we do in the rest of CLI ops that use the JobsRunner

--- src/org/opendatakit/briefcase/operations/ImportFromODK.java
+++ src/org/opendatakit/briefcase/operations/ImportFromODK.java
@@ -16,16 +16,23 @@
 package org.opendatakit.briefcase.operations;
 
 import static java.util.stream.Collectors.toList;
+import static org.opendatakit.briefcase.model.form.FormMetadataCommands.updateAsPulled;
 import static org.opendatakit.briefcase.operations.Common.FORM_ID;
 import static org.opendatakit.briefcase.operations.Common.STORAGE_DIR;
+import static org.opendatakit.briefcase.reused.job.Job.run;
 
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Optional;
+import org.bushe.swing.event.EventBus;
 import org.opendatakit.briefcase.model.FormStatus;
+import org.opendatakit.briefcase.model.TerminationFuture;
 import org.opendatakit.briefcase.model.form.FileSystemFormMetadataAdapter;
+import org.opendatakit.briefcase.model.form.FormKey;
 import org.opendatakit.briefcase.model.form.FormMetadataPort;
+import org.opendatakit.briefcase.pull.PullEvent;
 import org.opendatakit.briefcase.reused.BriefcaseException;
+import org.opendatakit.briefcase.reused.job.JobsRunner;
 import org.opendatakit.briefcase.transfer.TransferForms;
 import org.opendatakit.briefcase.util.FileSystemUtils;
 import org.opendatakit.briefcase.util.FormCache;
@@ -67,6 +74,22 @@ public class ImportFromODK {
     if (formId.isPresent() && forms.isEmpty())
       throw new BriefcaseException("Form " + formId.get() + " not found");
 
-    TransferFromODK.pull(briefcaseDir, odkDir, forms);
+    JobsRunner.launchAsync(forms.map(form -> {
+      TransferFromODK action = new TransferFromODK(briefcaseDir, odkDir.toFile(), new TerminationFuture(), TransferForms.of(form));
+      return run(jobStatus -> {
+        try {
+          boolean success = action.doAction();
+          if (success) {
+            EventBus.publish(PullEvent.Success.of(form));
+            formMetadataPort.execute(updateAsPulled(FormKey.from(form), briefcaseDir, form.getFormDir(briefcaseDir)));
+          } // TODO Originally there was no explicit side effect on non successful individual pulls. We might want to give some feedback
+        } catch (Exception e) {
+          // This will lift any checked exception thrown by the underlying code
+          // into a BriefcaseException that is managed by the error management
+          // flow driven by the Launcher class
+          throw new BriefcaseException("Failed to pull form (legacy)", e);
+        }
+      });
+    })).onComplete(() -> EventBus.publish(new PullEvent.PullComplete())).waitForCompletion();
   }
 }
