a1a31e41b83493bc803850ee0c5b87cc36d03119
Guille <ggalmazor@gmail.com>
2019-11-26 11:05:34 +0100

    Restore cancellation of the pull from collect process
    By coupling the action creation back to the jobsRunner object, we can use it as the TerminationFuture so that legacy code can check the runner's status and decide whether the job was cancelled mid-air.

--- src/org/opendatakit/briefcase/pull/PullFromCollect.java
+++ src/org/opendatakit/briefcase/pull/PullFromCollect.java
@@ -5,7 +5,6 @@ import static org.opendatakit.briefcase.reused.job.Job.run;
 
 import java.nio.file.Path;
 import java.util.function.Consumer;
-import org.opendatakit.briefcase.model.TerminationFuture;
 import org.opendatakit.briefcase.model.form.FormKey;
 import org.opendatakit.briefcase.model.form.FormMetadataPort;
 import org.opendatakit.briefcase.reused.BriefcaseException;
@@ -15,22 +14,20 @@ import org.opendatakit.briefcase.util.TransferFromODK;
 
 public class PullFromCollect {
   public static JobsRunner pullForms(FormMetadataPort formMetadataPort, TransferForms forms, Path briefcaseDir, Path collectDir, Consumer<PullEvent> onEvent) {
-    return JobsRunner.launchAsync(forms.map(form -> {
-      TransferFromODK action = new TransferFromODK(briefcaseDir, collectDir.toFile(), new TerminationFuture(), TransferForms.of(form));
-      return run(jobStatus -> {
-        try {
-          boolean success = action.doAction();
-          if (success) {
-            onEvent.accept(PullEvent.Success.of(form));
-            formMetadataPort.execute(updateAsPulled(FormKey.from(form), briefcaseDir, form.getFormDir(briefcaseDir)));
-          } // TODO Originally there was no explicit side effect on non successful individual pulls. We might want to give some feedback
-        } catch (Exception e) {
-          // This will lift any checked exception thrown by the underlying code
-          // into a BriefcaseException that is managed by the error management
-          // flow driven by the Launcher class
-          throw new BriefcaseException("Failed to pull form (legacy)", e);
-        }
-      });
-    })).onComplete(() -> onEvent.accept(new PullEvent.PullComplete()));
+    return JobsRunner.launchAsync(forms.map(form -> run(jobStatus -> {
+      TransferFromODK action = new TransferFromODK(briefcaseDir, collectDir.toFile(), jobStatus, TransferForms.of(form));
+      try {
+        boolean success = action.doAction();
+        if (success) {
+          onEvent.accept(PullEvent.Success.of(form));
+          formMetadataPort.execute(updateAsPulled(FormKey.from(form), briefcaseDir, form.getFormDir(briefcaseDir)));
+        } // TODO Originally there was no explicit side effect on non successful individual pulls. We might want to give some feedback
+      } catch (Exception e) {
+        // This will lift any checked exception thrown by the underlying code
+        // into a BriefcaseException that is managed by the error management
+        // flow driven by the Launcher class
+        throw new BriefcaseException("Failed to pull form (legacy)", e);
+      }
+    }))).onComplete(() -> onEvent.accept(new PullEvent.PullComplete()));
   }
 }
