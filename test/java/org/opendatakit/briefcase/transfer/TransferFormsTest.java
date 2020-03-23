722ba46434e43b5c6b1601dd2b817631536471e5
Elliott Wallace <elliott.d.wallace@gmail.com>
2020-03-23 09:07:05 +0100

    Add tests for TranferForms

--- test/java/org/opendatakit/briefcase/transfer/TransferFormsTest.java
+++ test/java/org/opendatakit/briefcase/transfer/TransferFormsTest.java
@@ -16,14 +16,34 @@
 
 package org.opendatakit.briefcase.transfer;
 
+import static java.nio.file.Paths.get;
 import static java.util.Collections.singletonList;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Before;
 import org.junit.Test;
+import org.opendatakit.briefcase.model.FormStatus;
 import org.opendatakit.briefcase.model.FormStatusBuilder;
+import org.opendatakit.briefcase.model.TestFormDefinition;
 
 public class TransferFormsTest {
+
+  private List<FormStatus> forms;
+  private Path briefcaseDir;
+
+  @Before
+  public void setUp() {
+    briefcaseDir = get("/storage/directory");
+    forms = new ArrayList<FormStatus>();
+    forms = FormStatusBuilder.buildFormStatusList(10);
+  }
+
   @Test
   public void empty_forms_can_merge_forms() {
     // What we are really testing is that no UnsupportedOperationException
@@ -41,5 +61,28 @@ public class TransferFormsTest {
     TransferForms forms = TransferForms.of(FormStatusBuilder.buildFormStatus(1));
     forms.clear();
     assertThat(forms.isEmpty(), is(true));
+  }
+
+  @Test
+  public void loads_all_forms() {
+    TransferForms transferForms = TransferForms.empty();
+    transferForms.load(forms);
+    assertEquals(10, transferForms.size());
+  }
+
+  @Test
+  public void test_select_all() {
+    TransferForms transferForms = TransferForms.from(forms);
+    transferForms.selectAll();
+    assertTrue(transferForms.allSelected());
+    assertTrue(transferForms.someSelected());
+  }
+
+  @Test
+  public void get_selected_forms() {
+    TransferForms transferForms = TransferForms.from(forms);
+    transferForms.selectAll();
+    transferForms.getSelectedForms();
+    assertEquals(10, transferForms.size());
   }
 }
