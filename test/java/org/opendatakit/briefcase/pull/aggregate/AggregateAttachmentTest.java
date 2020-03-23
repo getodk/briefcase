8af120afdd3dc5c94d91c8eda3dd364265ab1cd1
Patrick R <45533939+pareichlin@users.noreply.github.com>
2020-03-23 12:05:12 +0100

    Increased Branch Coverage in AggregateAttachment

--- test/java/org/opendatakit/briefcase/pull/aggregate/AggregateAttachmentTest.java
+++ test/java/org/opendatakit/briefcase/pull/aggregate/AggregateAttachmentTest.java
@@ -1,13 +1,15 @@
 package org.opendatakit.briefcase.pull.aggregate;
 
-import static org.junit.Assert.assertThat;
-
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import org.hamcrest.Matchers;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class AggregateAttachmentTest {
   @Test
@@ -15,5 +17,23 @@ public class AggregateAttachmentTest {
     Path file = Paths.get(AggregateAttachmentTest.class.getResource("/org/opendatakit/briefcase/pull/aggregate/lorem-ipsum-40k.txt").toURI());
     String expectedHash = "E79C0D4AD451003BA8CFDC1183AC89E9";
     assertThat(AggregateAttachment.md5(file), Matchers.is(expectedHash));
+  }
+
+  @Test
+  public void gets_download_url() throws URISyntaxException, MalformedURLException {
+    AggregateAttachment testAttachment = AggregateAttachment.of("file_1", "123456", "file://org/opendatakit/briefcase/pull/aggregate/lorem-ipsum-40k.txt");
+    URL expectedUrl = new URL("file://org/opendatakit/briefcase/pull/aggregate/lorem-ipsum-40k.txt");
+    assertEquals(expectedUrl, testAttachment.getDownloadUrl());
+  }
+
+  @Test
+  public void checks_equality_of_attachment() throws URISyntaxException, MalformedURLException {
+    AggregateAttachment testAttachment = AggregateAttachment.of("file_1", "123456", "file://org/opendatakit/briefcase/pull/aggregate/lorem-ipsum-40k.txt");
+    AggregateAttachment testAttachment2 = AggregateAttachment.of("file_1", "123456", "file://org/opendatakit/briefcase/pull/aggregate/lorem-ipsum-40k.txt");
+    AggregateAttachment testAttachment3 = AggregateAttachment.of("file_3", "1234567", "file://org/opendatakit/briefcase/lorem-ipsum-40k.txt");
+    assertFalse(testAttachment.equals(null));
+    assertTrue(testAttachment.equals(testAttachment));
+    assertTrue(testAttachment.equals(testAttachment2));
+    assertFalse(testAttachment.equals(testAttachment3));
   }
 }
