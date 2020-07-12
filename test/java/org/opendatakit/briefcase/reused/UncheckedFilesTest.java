81ef423b734035bf46a03432d336ce9d4d30b048
Tim Weerakoon <tim.weer@gmail.com>
2020-07-12 17:26:45 -0400

    Fixed checkstyle issues with tests

--- test/java/org/opendatakit/briefcase/reused/UncheckedFilesTest.java
+++ test/java/org/opendatakit/briefcase/reused/UncheckedFilesTest.java
@@ -1,8 +1,10 @@
 package org.opendatakit.briefcase.reused;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.opendatakit.briefcase.reused.UncheckedFiles.createFile;
+import static org.opendatakit.briefcase.reused.UncheckedFiles.createTempDirectory;
+import static org.opendatakit.briefcase.reused.UncheckedFiles.deleteRecursive;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,52 +12,54 @@ import java.io.UncheckedIOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static org.opendatakit.briefcase.reused.UncheckedFiles.*;
-import static org.mockito.Mockito.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 
 public class UncheckedFilesTest {
-    private static Path tempDir;
-    private Path temp;
+  private static Path tempDir;
+  private Path temp;
 
 
-    @Before
-    public void setUp() {
-        tempDir = createTempDirectory("briefcase");
-        temp = Paths.get(tempDir.toString() + "/test.txt");
-        createFile(temp);
-    }
+  @Before
+  public void setUp() {
+    tempDir = createTempDirectory("briefcase");
+    temp = Paths.get(tempDir.toString() + "/test.txt");
+    createFile(temp);
+  }
 
-    @After
-    public void tearDown() {
-        deleteRecursive(tempDir);
-    }
+  @After
+  public void tearDown() {
+    deleteRecursive(tempDir);
+  }
 
-    @Test(expected = UncheckedIOException.class)
-    public void newInputStream_should_throw_exception() {
-        UncheckedFiles.delete(temp);
-        UncheckedFiles.newInputStream(Paths.get(tempDir.toString() + "/test.txt"));
-    }
+  @Test(expected = UncheckedIOException.class)
+  public void newInputStream_should_throw_exception() {
+    UncheckedFiles.delete(temp);
+    UncheckedFiles.newInputStream(Paths.get(tempDir.toString() + "/test.txt"));
+  }
 
-    @Test
-    public void closeInputStream_should_handle_null() {
-        UncheckedFiles.closeInputStream(null);
-    }
+  @Test
+  public void closeInputStream_should_handle_null() {
+    UncheckedFiles.closeInputStream(null);
+  }
 
-    @Test(expected = IOException.class)
-    public void closeInputStream_should_close() throws IOException {
-        InputStream inputStream = UncheckedFiles.newInputStream(Paths.get(tempDir.toString() + "/test.txt"));
-        // close inputStream
-        UncheckedFiles.closeInputStream(inputStream);
+  @Test(expected = IOException.class)
+  public void closeInputStream_should_close() throws IOException {
+    InputStream inputStream = UncheckedFiles.newInputStream(Paths.get(tempDir.toString() + "/test.txt"));
+    // close inputStream
+    UncheckedFiles.closeInputStream(inputStream);
 
-        // after close, .available() should throw exception
-        inputStream.available();
-    }
+    // after close, .available() should throw exception
+    inputStream.available();
+  }
 
-    @Test(expected = UncheckedIOException.class)
-    public void closeInputStream_should_throw_exception() throws IOException {
-        InputStream inputStream = mock(InputStream.class);
-        doThrow(new IOException()).when(inputStream).close();
+  @Test(expected = UncheckedIOException.class)
+  public void closeInputStream_should_throw_exception() throws IOException {
+    InputStream inputStream = mock(InputStream.class);
+    doThrow(new IOException()).when(inputStream).close();
 
-        UncheckedFiles.closeInputStream(inputStream);
-    }
+    UncheckedFiles.closeInputStream(inputStream);
+  }
 }
